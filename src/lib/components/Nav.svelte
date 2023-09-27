6cdd71fa9bfdd08b89f5ae66648ca684adb9d7a5
Belem Zhang <belem.zhang@intel.com>
2023-09-27 15:06:57 +0800

    url refine for home links

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,9 +1,39 @@
 <script>
 	import { base } from '$app/paths';
+	import { page } from '$app/stores';
+	import { getModelIdfromPath } from '$lib/assets/js/utils';
+	import { beforeUpdate } from 'svelte';
+
+	let url = base;
+	$: search = $page.url.search;
+	$: fullSearch = search;
+
+	const homeUrl = () => {
+		let model = getModelIdfromPath();
+		let host = $page.url.protocol + '//' + $page.url.hostname;
+		let port = $page.url.port;
+		if (port) {
+			host = host + ':' + port;
+		}
+		if ($page.url.pathname.indexOf('web-ai-benchmark') > -1) {
+			host = host + '/web-ai-benchmark';
+		}
+
+		if ($page.url.pathname.indexOf('/run/') > -1) {
+			url = `${host}${search}&model=${model}`;
+		} else {
+			url = `${host}${fullSearch}`;
+		}
+	};
+	beforeUpdate(() => {
+		if ($page.url.pathname.indexOf('/run/') > -1) {
+			fullSearch = search + '&model=' + getModelIdfromPath();
+		}
+	});
 </script>
 
-<nav data-sveltekit-reload>
-	<a href="{base}/">home</a>
-	<a href="{base}/tests">tests</a>
-	<a href="{base}/about">about</a>
+<nav>
+	<a href={url} on:click={homeUrl}>home</a>
+	<a href="{base}/tests{fullSearch}">tests</a>
+	<a href="{base}/about{fullSearch}">about</a>
 </nav>
