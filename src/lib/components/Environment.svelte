2f661edf913546774062206f8edfa2df41416868
Belem Zhang <belem.zhang@intel.com>
2023-10-20 07:56:55 +0800

    memory

--- src/lib/components/Environment.svelte
+++ src/lib/components/Environment.svelte
@@ -86,14 +86,17 @@
 	let memory;
 
 	const runMemoryMeasurements = () => {
-		const interval = 1000;
+		const interval = 5 * 1000;
 		// 1 second
 		setTimeout(measureMemory, interval);
 	};
 
 	const measureMemory = async () => {
 		memory = await performance.measureUserAgentSpecificMemory();
-		console.log(memory);
+		if (memory) {
+			memory = memory / (1024 * 1024.0);
+			console.log(memory);
+		}
 		runMemoryMeasurements();
 	};
 
@@ -210,7 +213,7 @@
 				d="M240-360h80v-240h-80v240Zm200 0h80v-240h-80v240Zm200 0h80v-240h-80v240Zm-480 80h640v-400H160v400Zm0 0v-400 400Zm40 160v-80h-40q-33 0-56.5-23.5T80-280v-400q0-33 23.5-56.5T160-760h40v-80h80v80h160v-80h80v80h160v-80h80v80h40q33 0 56.5 23.5T880-680v400q0 33-23.5 56.5T800-200h-40v80h-80v-80H520v80h-80v-80H280v80h-80Z"
 			/></svg
 		>
-		{memory}
+		{memory} MB
 	{/if}
 
 	{#if (connectionType && connectionType !== 'none' && connectionType !== 'unknown' && connectionType !== 'other') || connectionEffectiveType}
