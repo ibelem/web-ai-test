fcee69b12aadbf8bb8d1a40f215c698726798de9
Belem Zhang <belem.zhang@intel.com>
2023-10-19 12:57:12 +0800

    WebNN CPU 4

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,3 +5,9 @@
 	new web standard that allows web apps and frameworks to accelerate deep neural networks with on-device
 	hardware such as GPUs, CPUs, or purpose-built AI accelerators.
 </div>
+
+<style>
+	div {
+		margin-bottom: 20px;
+	}
+</style>
