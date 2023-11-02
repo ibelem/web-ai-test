30a81c18118d8978548cba6e0a003be56a5231e5
Belem Zhang <belem.zhang@intel.com>
2023-11-02 14:10:54 +0800

    ncu package.json update

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,17 +1,22 @@
-<h1>About</h1>
+<div class="page">
+	<h1>About</h1>
 
-<div>
-	<a href="https://webmachinelearning.github.io/webnn-intro/">Web Neural Network API (WebNN)</a> is a
-	new web standard that allows web apps and frameworks to accelerate deep neural networks with on-device
-	hardware such as GPUs, CPUs, or purpose-built AI accelerators.
-</div>
+	<div>
+		<a href="https://webmachinelearning.github.io/webnn-intro/">Web Neural Network API (WebNN)</a> is
+		a new web standard that allows web apps and frameworks to accelerate deep neural networks with on-device
+		hardware such as GPUs, CPUs, or purpose-built AI accelerators.
+	</div>
 
-<div>
-	<span class="red">Not ready for formal measurement usage</span>, please contact
-	belem&commat;163.com for more details.
+	<div>
+		<span class="red">Not ready for formal measurement usage</span>, please contact
+		belem&commat;163.com for more details.
+	</div>
 </div>
 
 <style>
+	.page {
+		margin: 0 6px;
+	}
 	div {
 		margin-bottom: 20px;
 	}
