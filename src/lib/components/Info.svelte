324522c53c3e9466e4ac16e18db2f19517277906
Belem Zhang <belem.zhang@intel.com>
2023-11-14 14:37:33 +0800

    better progress and status

--- src/lib/components/Info.svelte
+++ src/lib/components/Info.svelte
@@ -6,7 +6,7 @@
 		testQueueStore,
 		testQueueLengthStore
 	} from '$lib/store/store';
-	import { getModelIdfromPath, getModelNameById } from '../../lib/assets/js/utils';
+	import { getModelIdfromPath, getModelNameById } from '$lib/assets/js/utils';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 
@@ -92,9 +92,9 @@
 			{testQueue[0].model}
 		</div>
 		<div class="s">
-			{testQueue[0].modeltype} · {testQueue[0].datatype} · {#if numOfRuns === 1}
+			{testQueue[0].modeltype} · {testQueue[0].datatype} · {testQueue[0].backend} · {#if numOfRuns === 1}
 				1 run
-			{:else}{numOfRuns} runs{/if} · {testQueue[0].backend}
+			{:else}{numOfRuns} runs{/if}
 			{#if getLoaded(id)}
 				·
 				{#if getProgress(id) === '100.0'}
@@ -110,33 +110,98 @@
 				{info.slice(-1)}
 			</div>
 		{/if}
-		<div>
-			<span>
-				{testQueueLength - testQueue.length}/{testQueueLength}
-				{percentageTestQueue}%</span
-			>
+		<div class="g2">
+			<div class="next">
+				{#if testQueue[1]}
+					Next: {testQueue[1].model} · {testQueue[1].modeltype} · {testQueue[1].datatype} · {testQueue[1]
+						.backend}
+				{/if}
+			</div>
+			<div class="progressinfo">
+				Test queue: {testQueueLength - testQueue.length}/{testQueueLength}
+				{percentageTestQueue}% completed
+			</div>
 		</div>
+		<div style="background-color:var(--green); width: {percentageTestQueue}%;" class="bar"></div>
 	</div>
 {/if}
 
 <style>
 	.info {
 		position: absolute;
-		top: 50%;
+		top: 40%;
 		left: 50%;
 		transform: translate(-50%, -50%);
-		padding: 10px;
+		padding: 0;
 		color: var(--font);
 		text-align: center;
-		max-width: 60%;
+		width: 50vw;
+		border: 1px solid var(--grey-02);
+	}
+
+	.g2 {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-template-rows: 1fr;
+		grid-row-gap: 0px;
+		margin-top: 20px;
+		padding: 0 10px;
+	}
+
+	.bar {
+		height: 1px;
+		margin-top: 10px;
+	}
+
+	.infodetails {
+		padding: 0;
+		overflow: hidden;
+		color: var(--red);
 	}
 
 	.info .s {
 		text-align: center;
-		margin-bottom: 10px;
+		margin-bottom: 20px;
 	}
 
 	.info .title {
 		color: var(--red);
+		margin-top: 10px;
+	}
+
+	.progressinfo {
+		justify-self: right;
+	}
+
+	.next {
+		justify-self: left;
+	}
+
+	@media (max-width: 1024px) {
+		.info {
+			width: 66vw;
+		}
+	}
+
+	@media (max-width: 900px) {
+		.info {
+			width: 75vw;
+		}
+	}
+
+	@media (max-width: 800px) {
+		.info {
+			width: 86vw;
+		}
+	}
+
+	@media (max-width: 512px) {
+		.info {
+			width: 98vw;
+		}
+		.g2 {
+			display: block;
+			padding: 0 10px;
+		}
 	}
 </style>
