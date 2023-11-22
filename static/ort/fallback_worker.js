fd3fcdd27011f7558cbb343919ae0d048c6a6a10
Belem Zhang <belem.zhang@intel.com>
2023-11-22 17:34:57 +0800

    fallback info - wip

--- static/ort/fallback_worker.js
+++ static/ort/fallback_worker.js
@@ -169,41 +169,56 @@ self.addEventListener('message', async (event) => {
       let filteredEverything = everything.filter(item => {
         return (
           String(item).includes("Operator type") ||
-          String(item).includes("GetCapability")
+          String(item).includes("GetCapability") ||
+          String(item).includes("is not supported for now")
           // || String(item).includes("Node(s) placed on")
         );
       });
+
       filteredEverything = filteredEverything.filter(item => !item.includes("is supported by browser"));
       filteredEverything = filteredEverything.filter(item => !item.includes("current supported node group size"));
 
       // Remove strings before "Operator type: "
       let cleanedJsonOperatorType = filteredEverything.map(item => {
-        const match = item.match(/Operator type: .*/);
+        const match = String(item).match(/Operator type: .*/);
         return match ? match[0] : item;
       });
 
       // Remove strings before "WebNNExecutionProvider::GetCapability,"
       let cleanedJsonWebNN = cleanedJsonOperatorType.map(item => {
-        const match = item.match(/WebNNExecutionProvider::GetCapability, .*/);
+        const match = String(item).match(/WebNNExecutionProvider::GetCapability, .*/);
         return match ? match[0] : item;
       });
 
-      let removedOperatorType = cleanedJsonWebNN.map(item => item.replace("Operator type: ", ""));
+      let removedHasSupportedInputsImpl = cleanedJsonWebNN.map(item => {
+        const match = String(item).match(/HasSupportedInputsImpl], .*/);
+        return match ? match[0] : item;
+      });
+
+      let removedHasSupportedInputsImplBefore = removedHasSupportedInputsImpl.map(item => {
+        let indexOfReshape = item.indexOf("base_op_builder.cc:90");
+        return indexOfReshape !== -1 ? item.substring(indexOfReshape) : item;
+      });
+
+      let removedBaseOpBuilder = removedHasSupportedInputsImplBefore.map(item => String(item).replace("base_op_builder.cc:90 ", ""));
+
+      let removedOperatorType = removedBaseOpBuilder.map(item => String(item).replace("Operator type: ", ""));
 
       // Remove "WebNNExecutionProvider::GetCapability, " from each item
       let removedWebNN = removedOperatorType.map(item => item.replace("WebNNExecutionProvider::GetCapability, ", ""));
       let removedCenter = removedWebNN.map(item => item.replace(/index: \[.*?\] supported: /, 'index: [] supported: '));
       let removeTail = removedCenter.map(item => item.replace('\u001b[m', ''));
+      let removeNewTail = removeTail.map(item => item.replace('HasSupportedInputsImpl]', ''));
 
       // Find the index of the first "number of" item
-      let indexOfNumber = removeTail.findIndex(item => item.startsWith("number of"));
+      let indexOfNumber = removeNewTail.findIndex(item => item.startsWith("number of"));
 
       // Remove the item starting with "number of" and its preceding items
       if (indexOfNumber !== -1) {
-        removeTail = removeTail.slice(indexOfNumber + 1);
+        removeNewTail = removeNewTail.slice(indexOfNumber + 1);
       }
 
-      let modifiedT = removeTail.flatMap(item => {
+      let modifiedT = removeNewTail.flatMap(item => {
         if (item.startsWith("number of")) {
           return item.split("number of").filter(Boolean).map(part => `number of ${part.trim()}`);
         } else {
@@ -219,7 +234,8 @@ self.addEventListener('message', async (event) => {
         "not_supported": [],
         "partitions_supported_by_webnn": 0,
         "nodes_in_the_graph": 0,
-        "nodes_supported_by_webnn": 0
+        "nodes_supported_by_webnn": 0,
+        "input_type_not_supported": []
       };
 
       modifiedT.forEach(item => {
@@ -233,14 +249,14 @@ self.addEventListener('message', async (event) => {
           obj["nodes_in_the_graph"] = parseInt(item.split(": ")[1]);
         } else if (item.startsWith("number of nodes supported by WebNN: ")) {
           obj["nodes_supported_by_webnn"] = parseInt(item.split(": ")[1]);
+        } else if (item.endsWith("is not supported for now")) {
+          let op = item.split("Input type: ")[0].trim().replace('[', '').replace(']', '');
+          let datatype = item.split("Input type: ")[1].replace('is not supported for now', '').trim().replace('[', '').replace(']', '')
+          obj["input_type_not_supported"].push(`${op}: ${datatype}`);
         }
       });
-
-      console.log(obj);
-
       self.postMessage(`Backend: WebNN_${backend.toUpperCase()}`);
       self.postMessage(obj);
-
     }
   }
 
