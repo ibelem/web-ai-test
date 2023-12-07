1a09b6bf59e46bc7f586992edd676375ad1ebe1f
Belem Zhang <belem.zhang@intel.com>
2023-12-07 08:31:51 +0800

    tiny

--- src/routes/fallback/+page.svelte
+++ src/routes/fallback/+page.svelte
@@ -341,7 +341,7 @@
 
 <style>
 	.tqtitle {
-		margin: 0;
+		margin: 10px 0 0 0;
 	}
 
 	.subtitle {
@@ -461,5 +461,11 @@
 
 	.progress .next {
 		justify-self: right;
+	}
+
+	@media (max-width: 512px) {
+		.f button {
+			width: 46.8vw;
+		}
 	}
 </style>
