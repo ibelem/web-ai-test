3cb230436ca984a975a801b23126605110e0ed06
Belem Zhang <belem.zhang@intel.com>
2024-01-23 15:48:20 +0800

    ui

--- src/routes/demo/+page.svelte
+++ src/routes/demo/+page.svelte
@@ -2,6 +2,8 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Environment from '$lib/components/Environment.svelte';
+	import Clock from '$lib/components/svg/Clock.svelte';
+	import Onnx from '$lib/components/svg/Onnx.svelte';
 </script>
 
 <Header />
@@ -11,11 +13,15 @@
 		<div class="title tq">Demo</div>
 	</div>
 
-	<div>
-		<a href="../stable-diffusion/sync.html">Stable Diffusion · UNet 1.5 · Sync</a>
-	</div>
-	<div>
-		<a href="../stable-diffusion/index.html">Stable Diffusion · UNet 1.5 · Async</a>
+	<div class="tq benchmark">
+		<div class="q tests" title="">
+			<div class="onnx"><Onnx /></div>
+			<a href="../stable-diffusion/index.html">Stable Diffusion · UNet 1.5 · Async</a>
+		</div>
+		<div class="q tests" title="">
+			<div class="onnx"><Onnx /></div>
+			<a href="../stable-diffusion/sync.html">Stable Diffusion · UNet 1.5 · Sync</a>
+		</div>
 	</div>
 </div>
 
@@ -27,8 +33,15 @@
 	.page {
 		margin: 0 6px;
 	}
-	div {
-		margin-bottom: 20px;
+
+	.onnx {
+		margin-bottom: 4px;
+	}
+
+	.tq .tests {
+		display: flex;
+		align-items: center;
+		padding: 4px 10px;
 	}
 
 	.red {
