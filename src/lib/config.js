8ba23731fe7cbceaab95dd7a438769113ef858eb
Belem Zhang <belem.zhang@intel.com>
2024-03-11 12:47:13 +0800

    webgpu 0311

--- src/lib/config.js
+++ src/lib/config.js
@@ -22,11 +22,11 @@ export const corsSites = [
 
 export const ortDists = {
   webgpu: {
-    version: 'v1.18 Internal 03-11-2024',
+    version: 'internal.20240311',
     url: 'https://ibelem.github.io/onnxruntime-web-dist/webgpu/ort.webgpu.min.js'
   },
   webnn_webglfix_wasm: {
-    version: 'v1.18 Internal 02-02-2024',
+    version: 'internal.20240202',
     url: '../ort/ort.all.min.js'
   }
 }
